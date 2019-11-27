f47799989f88eb719f4bb009d7143d75bf8b5085
Muata Nkosi <mnkosi@fandm.edu>
2019-11-27 14:16:29 -0500

    Initial commit with new login screen

---
+++ screens/Login.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+
+// const Login = React.createClass({
+//   render: () => {
+//     return <Text>Hello</Text>;
+//   },
+// });
+
+export default class Login extends Component {
+  state = {a: 'woah'};
+  render() {
+    return <Text>Hello</Text>;
+  }
+}
