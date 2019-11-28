257be3df797c136b500291b253ed40f282b81fe8
Muata Nkosi <mnkosi@fandm.edu>
2019-11-28 04:13:51 -0500

    Add logic to get user information and handle login

--- screens/Login.js
+++ screens/Login.js
@@ -9,6 +9,8 @@ import {
   ActivityIndicator,
 } from 'react-native';
 
+import Buffer from 'buffer';
+
 export default class Login extends Component {
   constructor(props) {
     super(props);
@@ -51,12 +53,40 @@ export default class Login extends Component {
   onLoginPressed = () => {
     console.log('Attempting to log in with username ' + this.state.username);
     this.setState({showProgress: true});
-    fetch('https://api.github.com/search/repositories?q=react')
+
+    const buffer = new Buffer.Buffer(
+      this.state.username + ':' + this.state.password,
+    );
+    const encodedAuth = buffer.toString('base64');
+
+    fetch('https://api.github.com/user', {
+      headers: {
+        Authorization: 'Basic ' + encodedAuth,
+      },
+    })
+      .then(response => {
+        if (response.status >= 200 && response.status < 300) {
+          return response;
+        }
+        if (response.status === 401) {
+          throw {
+            badCredentials: response.status === 401,
+            unknownError: response.status !== 401,
+          };
+        }
+        throw 'Unknown error';
+      })
       .then(response => {
         return response.json();
       })
       .then(results => {
         console.log(results);
+      })
+      .catch(err => {
+        console.log('login failed: ' + err);
+        this.setState(err);
+      })
+      .finally(() => {
         this.setState({showProgress: false});
       });
   };
